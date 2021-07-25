1da7f4eda16b3c3e50d28f7aafd9588fc3a54fbe
chaeyeon <gkscodus11_@frientrip.com>
2021-07-25 22:11:17 +0900

    calendar 테스트 추가

--- test/calendar.test.ts
+++ test/calendar.test.ts
@@ -2,14 +2,9 @@ import { CalendarService } from '../src/service/CalendarService';
 
 const calendarService = new CalendarService();
 
-describe('index test', () => {
-  it('test1', (done) => {
-    expect(1).toBe(1);
-    done();
-  });
-  it('test2', async (done) => {
-    const result = await calendarService.getCalendar();
-    console.log(result);
+describe('calendar test', () => {
+  it('getAcademiCalendar test - 1', async (done) => {
+    const result = await calendarService.getAcademicCalendar();
     expect(result).toBeTruthy();
     done();
   });
--- test/libraryNotice.test.ts
+++ test/libraryNotice.test.ts
@@ -2,12 +2,8 @@ import { NoticeService } from '../src/service/NoticeService';
 
 const noticeService = new NoticeService();
 
-describe('index test', () => {
-  it('test1', (done) => {
-    expect(1).toBe(1);
-    done();
-  });
-  it('test2', async (done) => {
+describe('notice test', () => {
+  it('getLibraryNotices test - 1', async (done) => {
     const result = await noticeService.getLibraryNotices();
     console.log(result);
     expect(result).toBeTruthy();
