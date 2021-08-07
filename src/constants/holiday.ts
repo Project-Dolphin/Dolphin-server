20890891ccb1647a59cfe0f540f3525f19fb5b27
kim deok hyeon <kdhmny@naver.com>
2021-08-07 16:21:18 +0900

    데이터 추가

--- src/constants/holiday.ts
+++ src/constants/holiday.ts
@@ -43,4 +43,20 @@ export const holiDay = [
         term: { startedAt: '2021-12-25', endedAt: '2021-12-25' },
         content: '크리스마스',
     },
+    {
+        term: { startedAt: '2021-08-16', endedAt: '2021-08-16' },
+        content: '광복절 대체휴무',
+    },
+    {
+        term: { startedAt: '2021-10-04', endedAt: '2021-10-04' },
+        content: '개천절 대체휴무',
+    },
+    {
+        term: { startedAt: '2021-10-11', endedAt: '2021-10-11' },
+        content: '한글날 대체휴무',
+    },
+    {
+        term: { startedAt: '2021-12-27', endedAt: '2021-12-27' },
+        content: '크리스마스 대체휴무',
+    },
   ];
