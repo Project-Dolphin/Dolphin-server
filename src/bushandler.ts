3ac66be3a0b3f018bf040e597f33fa3a387d50bb
Kim-deokhyeon <kdhmny@naver.com>
2021-08-10 00:25:49 +0900

    sry...

--- src/bushandler.ts
+++ src/bushandler.ts
@@ -4,31 +4,7 @@ import { BusService } from './service/BusService';
 const dolphin: Handler = async (event: APIGatewayEvent) => {
 
   const path = event.path;
-  const querystring = event.pathParameters ? event.pathParameters.bstopid : null
 
-  if (querystring == null) {
-    // 운행중인 모든 버스
-    const busService = new BusService();
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        data: busService.getAllNode(),
-        path: path,
-      }),
-    };
-  }
-
-  if (querystring != null) {
-    // 특정 정류장의 도착 정보
-    const busService = new BusService();
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        data: busService.getSpecificNode(querystring),
-        path: path,
-      }),
-    };
-  }
 
   return {
     statusCode: 200,
--- src/shuttlehandler.ts
+++ src/shuttlehandler.ts
@@ -4,29 +4,7 @@ import { ShuttleService } from './service/ShuttleService';
 const dolphin: Handler = async (event: APIGatewayEvent) => {
   const path = event.path;
 
-  if (path === '/shuttle/next') {
-    // 다음 셔틀
-    const shuttleService = new ShuttleService();
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        data: shuttleService.getNextShuttle(),
-        path: path,
-      }),
-    };
-  }
 
-  if (path === '/shuttle/today') {
-    // 그 날의 모든 셔틀
-    const shuttleService = new ShuttleService();
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        data: shuttleService.getDayShuttle(),
-        path: path,
-      }),
-    };
-  }
 
   return {
     statusCode: 200,
