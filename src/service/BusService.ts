cbbf6bb04b6749e3b52a99c42e68c0e7033afb7c
Kim-deokhyeon <kdhmny@naver.com>
2021-08-09 12:00:23 +0900

    log

--- src/service/BusService.ts
+++ src/service/BusService.ts
@@ -30,9 +30,10 @@ export class BusService {
 
     const response = await got.get(url);
     if (response.headers['resultCode'] == '99') return Promise.reject('세션 종료');
+    console.log(response.body);
 
-    const item = response.body ==
-      null ? { carNo1: "차량 없음", carNo2: "차량 없음", min1: 999, min2: 999, station1: 999, station2: 999, lowplate1: false, lowplate2: false } : JSON.stringify(response.body['items']);
+    const item = response.body == null
+      ? { carNo1: "차량 없음", carNo2: "차량 없음", min1: 999, min2: 999, station1: 999, station2: 999, lowplate1: false, lowplate2: false } : JSON.stringify(response.body['items']);
 
     console.log(item);
 
