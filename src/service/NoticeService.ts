81294b4af2de3142c14e7f6287091ef666715236
Kim-deokhyeon <kdhmny@naver.com>
2021-08-10 02:18:54 +0900

    origin

--- src/service/NoticeService.ts
+++ src/service/NoticeService.ts
@@ -11,34 +11,25 @@ export class NoticeService {
   private readonly url = 'https://www.kmou.ac.kr/kmou/main.do#notice';
   private readonly kmouUrl = 'https://www.kmou.ac.kr';
 
-
   public async getMainNotice(): Promise<Notice[]> {
     const rawText = await got.get(this.url);
     const root = parse(rawText.body);
-    const noticeHtmls = root.querySelectorAll('.notibox');
-    const noticeOn = noticeHtmls[0].querySelectorAll('.on');
-    const list = noticeOn[0].querySelector('.list_box');
-    console.log('list : ', list)
+
+    const noticeHtmls = root.querySelectorAll('.notibox.on');
+    const list = noticeHtmls[0].querySelector('.list_box');
+    const contents = list.querySelectorAll('li');
     const notices: Notice[] = [];
-    if (list != null) {
-      const contents = list.querySelectorAll('li'); // optional chaining
 
+    contents.forEach((content) => {
+      const titleData = content.querySelector('a');
+      const dateData = content.querySelector('span');
 
-      if (contents != null) {
-        contents.forEach((content) => {
-          const titleData = content.querySelector('a');
-          const dateData = content.querySelector('span');
-
-          notices.push({
-            title: titleData.rawText.trim(),
-            date: dateData.rawText.replace(/\./g, '-'),
-            link: this.kmouUrl.concat(titleData.attributes.href),
-          });
-        });
-      }
-    }
-
-    console.log('notices : ', notices);
+      notices.push({
+        title: titleData.rawText.trim(),
+        date: dateData.rawText.replace(/\./g, '-'),
+        link: this.kmouUrl.concat(titleData.attributes.href),
+      });
+    });
 
     return notices;
   }
