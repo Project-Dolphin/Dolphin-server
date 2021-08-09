08019dc879a6de7e764303ec24caa5137da06954
Kim-deokhyeon <kdhmny@naver.com>
2021-08-09 12:03:16 +0900

    test

--- src/service/BusService.ts
+++ src/service/BusService.ts
@@ -29,10 +29,12 @@ export class BusService {
     const url = `http://61.43.246.153/openapi-data/service/busanBIMS2/busStopArr?serviceKey=R3BdsX99pQj7YTLiUWzWoPMqBWqfOMg9alf9pGA88lx3tknpA5uE04cl0nMrXiCt3X%2BlUzTJ1Mwa8qZAxO6eZA%3D%3D&bstopid=${bstopid}&lineid=5200179000`;
 
     const response = await got.get(url);
+    var tObj = parser.getTraversalObj(response.body, options);
+    var jsonObj = parser.convertToJson(tObj, options);
     if (response.headers['resultCode'] == '99') return Promise.reject('세션 종료');
-    console.log(response.body);
+    console.log(response.body['items']);
 
-    const item = response.body == null
+    const item = jsonObj == null
       ? { carNo1: "차량 없음", carNo2: "차량 없음", min1: 999, min2: 999, station1: 999, station2: 999, lowplate1: false, lowplate2: false } : JSON.stringify(response.body['items']);
 
     console.log(item);
