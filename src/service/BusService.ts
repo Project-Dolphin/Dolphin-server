4e1d01e6b046fc5ce899f869a15596afc621c181
Kim-deokhyeon <kdhmny@naver.com>
2021-08-09 11:40:29 +0900

    190 전체 정보

--- src/service/BusService.ts
+++ src/service/BusService.ts
@@ -4,44 +4,44 @@ import request = require('request');
 import { options } from '../constants/option/xml_parser_option';
 
 interface BusArriveInfo {
-  carNo1 : Number;
-  carNo2 : Number;
-  min1 : Number;
-  min2 : Number;
-  station1 : Number;
-  station2 : Number;
-  lowplate1 : Boolean;
-  lowplate2 : Boolean;
+  carNo1: Number;
+  carNo2: Number;
+  min1: Number;
+  min2: Number;
+  station1: Number;
+  station2: Number;
+  lowplate1: Boolean;
+  lowplate2: Boolean;
 }
 
 interface BusInfo {
-  carNo : String;
-  gpsTm : String;
-  lat : Number;
-  lon : Number;
-  nodeId : Number;
+  carNo: String;
+  gpsTm: String;
+  lat: Number;
+  lon: Number;
+  nodeId: Number;
 }
 
 export class BusService {
 
-  public async getSpecificNode(bstopid : Number): Promise<BusArriveInfo> {
+  public async getSpecificNode(bstopid: Number): Promise<BusArriveInfo> {
 
     const url = `http://61.43.246.153/openapi-data/service/busanBIMS2/busStopArr?serviceKey=R3BdsX99pQj7YTLiUWzWoPMqBWqfOMg9alf9pGA88lx3tknpA5uE04cl0nMrXiCt3X%2BlUzTJ1Mwa8qZAxO6eZA%3D%3D&bstopid=${bstopid}&lineid=5200179000`;
 
     const response = await got.get(url);
-    if(response.headers['resultCode'] == '99') return Promise.reject('세션 종료');
+    if (response.headers['resultCode'] == '99') return Promise.reject('세션 종료');
 
     const item = JSON.stringify(response.body['items']['item']);
 
-    var arriveInfo :BusArriveInfo = {
+    var arriveInfo: BusArriveInfo = {
       carNo1: item['carNo1'],
-      carNo2 : item['carNo2'],
-      min1 : item['min1'],
-      min2 : item['min1'],
-      station1 : item['station1'],
-      station2 : item['station2'],
-      lowplate1 : item['lowplate'],
-      lowplate2 : item['lowplate'],
+      carNo2: item['carNo2'],
+      min1: item['min1'],
+      min2: item['min1'],
+      station1: item['station1'],
+      station2: item['station2'],
+      lowplate1: item['lowplate'],
+      lowplate2: item['lowplate'],
     };
     return arriveInfo;
   }
@@ -51,30 +51,31 @@ export class BusService {
     const url = `http://61.43.246.153/openapi-data/service/busanBIMS2/busInfoRoute?serviceKey=R3BdsX99pQj7YTLiUWzWoPMqBWqfOMg9alf9pGA88lx3tknpA5uE04cl0nMrXiCt3X%2BlUzTJ1Mwa8qZAxO6eZA%3D%3D&lineid=5200190000`;
     var arriveInfo: BusInfo[] = [];
 
-    await request({
+    request({
       url: url,
-      method: 'GET'
+      method: 'GET',
     }, function (error, response, body) {
       //console.log('Status', response.statusCode);
       //console.log('Headers', JSON.stringify(response.headers));
 
-  
-        if( parser.validate(body) === true) { 
-          var jsonObj = parser.parse(body,options);
-        }   
-        
-        var tObj = parser.getTraversalObj(body,options);
-        var jsonObj = parser.convertToJson(tObj,options);
-    
+      if (!error && response.statusCode === 200) {
+        if (parser.validate(body) === true) {
+          var jsonObj = parser.parse(body, options);
+        }
+
+        var tObj = parser.getTraversalObj(body, options);
+        var jsonObj = parser.convertToJson(tObj, options);
+
         var tmp = jsonObj.response.body.items.item;
-        tmp.forEach(function(value: any){
-            if(value.lat && value.lon) {
-                if(String(value.gpsTm).length!=6) value.gpsTm = "0"+value.gpsTm;
-                arriveInfo.push({carNo: value.carNo, nodeId: value.nodeId, lat: value.lat, lon: value.lon, gpsTm: value.gpsTm})
-            }
+        tmp.forEach(function (value: any) {
+          if (value.lat && value.lon) {
+            if (String(value.gpsTm).length != 6) value.gpsTm = "0" + value.gpsTm;
+            arriveInfo.push({ carNo: value.carNo, nodeId: value.nodeId, lat: value.lat, lon: value.lon, gpsTm: value.gpsTm })
+          }
         })
-        
-        console.log(arriveInfo)
+
+        console.log(arriveInfo);
+      }
     });
 
 
