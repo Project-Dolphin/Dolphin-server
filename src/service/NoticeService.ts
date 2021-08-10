f8f35a2f9ee28bcd176971b3a605c6b958c74609
Kim-deokhyeon <kdhmny@naver.com>
2021-08-10 11:31:28 +0900

    log

--- src/service/NoticeService.ts
+++ src/service/NoticeService.ts
@@ -8,53 +8,39 @@ interface Notice {
 }
 
 export class NoticeService {
-  //private readonly url = 'http://www.kmou.ac.kr/kmou/main.do';
+  private readonly url = 'http://www.kmou.ac.kr/kmou/main.do';
   private readonly kmouUrl = 'https://www.kmou.ac.kr';
 
   public async getMainNotice(): Promise<Notice[]> {
 
-    const infuncUrl = 'https://www.kmou.ac.kr/kmou/main.do'
 
     const notices: Notice[] = [];
 
-    const rawText = await got.get(infuncUrl);
+    const rawText = await got.get(this.url);
     const root = parse(rawText.body);
 
-    //#container > div.main_content > div.M_con2 > div.notice > div.notibox.on > div.list_box > ul
+    const noticeHtmls = root.querySelectorAll('.notibox');
+    const list = noticeHtmls[1].querySelector('.list_box');
+    if (list != null) {
+      const contents = list.querySelectorAll('li'); // optional chaining
 
-    const container = root?.querySelector("#container");
-    const mainContent = container?.querySelector(".main_content")
-    const con2 = mainContent?.querySelector(".M_con2")
-    const notice = con2?.querySelector(".notice")
-    const notibox = notice?.querySelector(".notibox");
+      if (contents != null) {
+        contents.forEach((content) => {
+          const titleData = content.querySelector('a');
+          const dateData = content.querySelector('span');
 
-    const list = notibox?.querySelector(".list_box")
-    const ul = list?.querySelector("ul")
-
-    /*const noticeHtmls = root.querySelector(".notice");
-    console.log('noticeHtmls : ', noticeHtmls);
-    const notiBox0 = noticeHtmls?.querySelector(":notibox .list_box")
-    console.log('notiBox0 : ', notiBox0)
-    const list = notiBox0?.querySelector('.list_box');*/
-    console.log('ul : ', ul);
-    if (ul != null) {
-      const contents = ul.querySelectorAll('li');
-
-      contents.forEach((content) => {
-        const titleData = content.querySelector('a');
-        const dateData = content.querySelector('span');
-
-        notices.push({
-          title: titleData.rawText.trim(),
-          date: dateData.rawText.replace(/\./g, '-'),
-          link: this.kmouUrl.concat(titleData.attributes.href),
+          notices.push({
+            title: titleData.rawText.trim(),
+            date: dateData.rawText.replace(/\./g, '-'),
+            link: this.kmouUrl.concat(titleData.attributes.href),
+          });
         });
-      });
-
+      };
     }
 
     console.log('notices : ', notices)
 
     return notices;
+
   }
 }
