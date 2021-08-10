22a3a1251464fe351c44ba5fbd6f1383185802d5
Kim-deokhyeon <kdhmny@naver.com>
2021-08-10 09:21:56 +0900

    Selector

--- src/service/NoticeService.ts
+++ src/service/NoticeService.ts
@@ -18,10 +18,12 @@ export class NoticeService {
     const rawText = await got.get(this.url);
     const root = parse(rawText.body);
 
+    //#container > div.main_content > div.M_con2 > div.notice > div.notibox.on > div.list_box
+
     /*const noticeHtmls = root.querySelector(".notice");
     const notiBox0 = noticeHtmls.querySelector(".notibox")
     console.log('noticeHtmls : ', noticeHtmls);*/
-    const list = root.querySelector('.notice.notibox.listbox');
+    const list = root.querySelector('notice notibox list_box');
     console.log(list);
     if (list != null) {
       const contents = list.querySelectorAll('li');
