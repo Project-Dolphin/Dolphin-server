ece487351089b4737c27c1a6cc7cec10902f962e
Kim deok hyeon <kdhmny@naver.com>
2021-12-05 17:18:32 +0900

    오타..sry

--- src/service/BusService.ts
+++ src/service/BusService.ts
@@ -114,7 +114,7 @@ export class BusService {
   }
 
   public async getAllNode(): Promise<BusInfo[]> {
-    const url = 'http://http://apis.data.go.kr/6260000/BusanBIMS/busInfoByRouteId?servicekey=R3BdsX99pQj7YTLiUWzWoPMqBWqfOMg9alf9pGA88lx3tknpA5uE04cl0nMrXiCt3X%2BlUzTJ1Mwa8qZAxO6eZA%3D%3D&lineid=5200190000';
+    const url = 'http://apis.data.go.kr/6260000/BusanBIMS/busInfoByRouteId?servicekey=R3BdsX99pQj7YTLiUWzWoPMqBWqfOMg9alf9pGA88lx3tknpA5uE04cl0nMrXiCt3X%2BlUzTJ1Mwa8qZAxO6eZA%3D%3D&lineid=5200190000';
 
 
     // var queryParams = '?' + 'ServiceKey' + '=' + this.serviceKey;
