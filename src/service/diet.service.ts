b3135820f46d72fac40d1d588102b80dc9edba61
mukjo96 <mukjo96@gmail.com>
2022-03-21 21:50:00 +0900

    modify : 식단 tbody 비어있는 경우 순서 꼬이는 문제 수정

--- src/service/diet.service.ts
+++ src/service/diet.service.ts
@@ -52,21 +52,88 @@ export class DietService {
   private readonly navalBaseUrl = 'http://badaro.kmou.ac.kr';
 
   async getSocietyDietAsync(): Promise<SocietyResultType[] | string> {
-    const results: SocietyResultType[] = [];
+    const results: SocietyResultType[] = [
+      {
+        type: 0,
+        value: '',
+      },
+      {
+        type: 1,
+        value: '',
+      },
+      {
+        type: 2,
+        value: '',
+      },
+      {
+        type: 3,
+        value: '',
+      },
+      {
+        type: 4,
+        value: '',
+      },
+      {
+        type: 5,
+        value: '',
+      },
+      {
+        type: 6,
+        value: '',
+      },
+      {
+        type: 7,
+        value: '',
+      },
+      {
+        type: 8,
+        value: '',
+      },
+    ];
     const result = await got.get(this.societyUrl);
     const rawBody = cheerio.load(result.body);
-    rawBody('body > div > div > div > div > section > div > div > div > form > div > table > tbody > tr > td').each(
-      (index, element) => {
-        results.push({
-          type: index + 2,
-          value: this.replaceSpecialCharacters(rawBody(element).html()?.toString() || ''),
+    rawBody('.detail_tb').each((index, element) => {
+      rawBody(element)
+        .find('tbody')
+        .each((ii, e) => {
+          rawBody(e)
+            .find('tr > td')
+            .each((i, el) => {
+              switch (index) {
+                case 0:
+                  results[i] = {
+                    type: i,
+                    value: this.replaceSpecialCharacters(rawBody(el).html()?.toString() || ''),
+                  };
+                  break;
+                case 1:
+                  results[i + 2] = {
+                    type: i + 2,
+                    value: this.replaceSpecialCharacters(rawBody(el).html()?.toString() || ''),
+                  };
+                  break;
+                case 2:
+                  results[i + 7] = {
+                    type: i + 2,
+                    value: this.replaceSpecialCharacters(rawBody(el).html()?.toString() || ''),
+                  };
+                  break;
+                default:
+                  break;
+              }
+            });
         });
-      },
-    );
-    // 에러 수정
-    results[0].type = 0;
-    results.splice(1, 0, { type: 1, value: '' });
-    results.splice(2, 0, { type: 2, value: '' });
+    });
+    // rawBody('body > div > div > div > div > section > div > div > div > form > div > table > tbody > tr > td').each((index, element) => {
+    //   results.push({
+    //     type: index + 2,
+    //     value: this.replaceSpecialCharacters(rawBody(element).html()?.toString() || ''),
+    //   });
+    // });
+    // // 에러 수정
+    // results[0].type = 0;
+    // results.splice(1, 0, { type: 1, value: '' });
+    // results.splice(2, 0, { type: 2, value: '' });
 
     if (results.length === 3 && results[0].value.includes('년')) {
       return 'DietService.getSocietyDietAsync: There are no any diet';
@@ -78,10 +145,10 @@ export class DietService {
   private replaceSpecialCharacters(content: string | null): string {
     return content
       ? content
-        .replace(/<br>/g, '\n')
-        .replace(/&nbsp;/g, ' ')
-        .replace(/&amp;/g, '&')
-        .trim()
+          .replace(/<br>/g, '\n')
+          .replace(/&nbsp;/g, ' ')
+          .replace(/&amp;/g, '&')
+          .trim()
       : '';
   }
 
@@ -141,6 +208,7 @@ export class DietService {
       rawBody('.detail_tb')
         .find('tbody > tr > td')
         .each((index, element) => {
+          console.log(index, element);
           if (rawBody(element).html()) {
             const html = rawBody(element).html() || '';
             menus.push(this.getConvertedMenus(html.toString()));
@@ -209,21 +277,19 @@ export class DietService {
     const rawBody = cheerio.load(result.body);
     const todayMMddFormat = DayJS().format('MM/DD').replace('0', '').replace('/0', '/');
     console.log('today: ', todayMMddFormat);
-    rawBody('div > section > section > div > div > div > div > div > table > tbody > tr > td').each(
-      (index, element) => {
-        if (foundToday && rawBody(element).html()?.startsWith('<strong>')) {
-          return false;
-        }
-        if (foundToday) {
-          results.push(rawBody(element).text());
-        }
-        if (rawBody(element).html()?.includes(todayMMddFormat)) {
-          foundToday = true;
-        }
+    rawBody('div > section > section > div > div > div > div > div > table > tbody > tr > td').each((index, element) => {
+      if (foundToday && rawBody(element).html()?.startsWith('<strong>')) {
+        return false;
+      }
+      if (foundToday) {
+        results.push(rawBody(element).text());
+      }
+      if (rawBody(element).html()?.includes(todayMMddFormat)) {
+        foundToday = true;
+      }
 
-        return true;
-      },
-    );
+      return true;
+    });
 
     if (!foundToday) {
       return 'DietService.getNavalDietAsync: There are no any diet';
