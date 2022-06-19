92e135dfe1a540239035762c6b3c9a0ce4bf3c21
chaeyeonhan1225 <gkscodus11@gmail.com>
2022-06-20 02:04:35 +0900

    feat: 시간별로 모든 식단 조회하는 기능 추가

--- src/routes/dietRouter.ts
+++ src/routes/dietRouter.ts
@@ -4,10 +4,10 @@ import { dietService } from '../service/DietService';
 const router = express.Router();
 
 router.get('/', async (req: Request, res: Response) => {
-  const at = req.query.at;
-  const where = req.query.where;
+  const at = req.query.at as string;
+  const where = req.query.where as string;
   
-  const diet = await dietService.getSocietyDiet();
+  const diet = await dietService.getAllDiet(at, where);
   return res.status(200).json(diet);
 });
 
